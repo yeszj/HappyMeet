c6055c98a67e49f010cd9fdf478d381814eab6bb
zhengjun <458384201@qq.com>
2026-02-12 15:02:55 +0800

    v1.5.2 送礼支持选择数量

--- function/baseLib/src/main/java/cn/yanhu/baselib/base/BaseSheetDialog.kt
+++ function/baseLib/src/main/java/cn/yanhu/baselib/base/BaseSheetDialog.kt
@@ -2,17 +2,22 @@ package cn.yanhu.baselib.base
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.FrameLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.luck.picture.lib.utils.DensityUtil.getNavigationBarHeight
 
 
 abstract class BaseSheetDialog<B : ViewBinding?> : BottomSheetDialogFragment() {
@@ -39,14 +44,39 @@ abstract class BaseSheetDialog<B : ViewBinding?> : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         dialog?.window?.let {
             WindowCompat.setDecorFitsSystemWindows(it, false)
+
         }
         dialog?.setOnShowListener { _: DialogInterface? ->
             (view.parent as ViewGroup).setBackgroundColor(Color.TRANSPARENT)
+            try {
+                val bottomSheet = dialog?.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+                bottomSheet?.let { sheet ->
+                    // 不改变高度，只加底部内边距
+                    val navBarHeight = getNavigationBarHeight(requireContext())
+                    sheet.setPadding(0, 0, 0, navBarHeight)
+
+                    // 确保内容不被压缩
+                    sheet.clipToPadding = false
+                    val behavior = BottomSheetBehavior.from(sheet)
+                    // 关键：强制触发重新布局
+                    sheet.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                        override fun onGlobalLayout() {
+                            sheet.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                            // 动画方式：快速展开再收回，触发高度重算
+                            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                        }
+                    })
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
         }
     }
 
     protected fun setOnApplyWindowInsets(view: View) {
         dialog?.window?.let {
+
             ViewCompat.setOnApplyWindowInsetsListener(it.decorView) { v: View?, insets: WindowInsetsCompat ->
                 val systemInset = insets.getInsets(WindowInsetsCompat.Type.systemBars())
                 view.setPadding(0, 0, 0, systemInset.bottom)
